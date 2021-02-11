32d4497124a132a6ca4fb90954fe2dc0bae3d23d
durga750 <78526756+durga750@users.noreply.github.com>
2021-02-11 14:27:04 +0530

    Add files via upload

---
+++ Pro1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphisam
+{
+    class Maths
+    {
+        public void AreaOfSquare(int side)
+        {
+            int area = side * side;
+            Console.WriteLine(area);
+        }
+
+        public void AreaOfRect(int length,int bredth)
+        {
+            int area = length * bredth;
+            Console.WriteLine(area);
+        }
+
+        public void AreaOfcircle(int radius)
+        {
+            float pi=3.141f;
+            float area = pi * radius * radius;
+            Console.WriteLine(area);
+        }
+    }
+    class Pro1
+    {
+        static void Main(string[] args)
+        {
+            Maths m = new Maths();
+            m.AreaOfSquare(5);
+            m.AreaOfRect(3,4);
+            m.AreaOfcircle(2);
+
+        }
+    }
+}
